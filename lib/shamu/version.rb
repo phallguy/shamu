591a10b7c7999e7a197fcc8e64aea9f136f53485
Paul Alexander <paul@phallguy.com>
2016-03-16 23:10:01 -0700

    Release 0.0.2

--- lib/shamu/version.rb
+++ lib/shamu/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Shamu
   # The primary version number
-  VERSION_NUMBER  = "0.0.1".freeze
+  VERSION_NUMBER  = "0.0.2".freeze
 
   # Version suffix such as 'beta' or 'alpha'
   VERSION_SUFFIX  = "".freeze
